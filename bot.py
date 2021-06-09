a8ccde3d280a17aebb9b277968d83bf1414a23c2
NAFISA ALAM <37190335+NafuKrypto@users.noreply.github.com>
2021-06-09 20:55:10 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ Created on Wed Jun  9 20:11:32 2021
 """
 
 from gtts import gTTS
-#import os
+ 
 language = 'en'
 with open('lyrics.txt') as f:
     lines = f.readlines()
@@ -20,9 +20,7 @@ for x in lines:
 #print(lines)
 print(text)
 output = gTTS(text=text, lang=language,slow=False)
-##text="Smooth like butter.Like a criminal undercover.Gon' pop like trouble.Breakin' into your heart like that.Cool shade stunner.Yeah, I owe it all to my mother.Hot like summer.Yeah, I'm makin' you sweat like that"
-
-
+ 
 
 
 
