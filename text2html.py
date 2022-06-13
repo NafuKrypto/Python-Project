533e8853124c174b7a50f450dd28b1513b638b7d
NAFISA ALAM <37190335+NafuKrypto@users.noreply.github.com>
2022-06-13 19:30:50 +0600

    Create text2html.py

---
+++ text2html.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 13 17:41:50 2022
+
+@author: hp
+"""
+
+import textile
+
+import tkinter as tk
+
+def printInput():
+    inp = T.get(1.0, "end-1c")
+    html = textile.textile( inp )
+    print("\nAfter converted to HTML: \n",html)
+    lbl.config(text = "Converted Html : "+html) 
+ 
+root = tk.Tk()
+ 
+# specify size of window.
+root.geometry("250x170")
+ 
+# Create text widget and specify size.
+T = tk.Text(root, height = 5, width = 52)
+
+# Create button for next text.
+T.pack()
+b1 = tk.Button(root, text = "Submit", command=printInput)
+b1.pack() 
+
+lbl = tk.Label(root, text = "")
+lbl.pack() 
+# Insert The Fact.
+T.insert(tk.END,"""""" )
+# Label Creation
+tk.mainloop()
